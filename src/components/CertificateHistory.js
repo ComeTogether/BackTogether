bb66de57d9ca42e3eec23f6acd27074c43ea8d02
NikosCha <NikosChaHellas@gmail.com>
2020-10-22 00:22:53 +0300

    new schema - qr improvements

--- src/components/CertificateHistory.js
+++ src/components/CertificateHistory.js
@@ -1,127 +1,121 @@
-import React from 'react';
-import { FlatList, SafeAreaView, Text, Platform, View, Image, ActivityIndicator, RefreshControl } from 'react-native';
-import { Test, Splash } from '../components';
+import React from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import { Test } from "../components";
 import firestore from "@react-native-firebase/firestore";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 
+const CertificateHistory = ({ navigation, userToken }) => {
+  const [cert, setCert] = React.useState(null);
+  const [wait, setWait] = React.useState(true);
 
-const CertificateHistory = ({navigation, userToken}) => {
+  let un = () => {};
 
-    const [cert, setCert] = React.useState(null);
-    const [wait, setWait] = React.useState(true)
-    const [refresh, setRefresh] = React.useState(false);
-    let un = () => {};
-
-    React.useEffect(()=>{
-      const subscriber = firestore()
+  React.useEffect(() => {
+    const query = firestore()
       .collection("tests")
-      .where('email', '==', userToken.email)
-      .get()
-      .then((res) => {
-        if (res.docs.length !== 0) {
-         un = firestore()
-            .collection("tests")
-            .doc(res.docs[0].ref.id)
-            .onSnapshot((documentSnapshot) => {
-              setCert(documentSnapshot.data().tests);
-            });
-        }
-        setWait(false);
+      .where("userId", "==", userToken.id);
 
+    un = query.onSnapshot((querySnapshot) => {
+      const data = [];
 
-      })
-      .catch((error) => {
-        alert( error)
-      })
-      return  () => un();
-    }, [])
+      querySnapshot.forEach((documentSnapshot) => {
+        data.push({
+          ...documentSnapshot.data(),
+        });
+      });
+      setCert(data);
+    });
 
-    const onRefresh = React.useCallback(() => {
-        setRefresh(true)
-        firestore()
-      .collection("tests")
-      .where('email', '==', userToken.email)
-      .get()
-      .then((res) => {
-        if (res.docs.length !== 0) {
-          firestore()
-            .collection("tests")
-            .doc(res.docs[0].ref.id)
-            .onSnapshot((documentSnapshot) => {
-              setCert(documentSnapshot.data().tests);
-            });
-          }
-      })
-      .then(()=>setRefresh(false))
-      .catch((error) => {
-        alert( error)
-      })
-    },[refresh])
+    setWait(false);
+    return () => un();
+  }, []);
 
-    const onSelect = React.useCallback((id, authority, issueDate, testType, result) => {
-      navigation.navigate('Summary',{id:id, authority:authority, issueDate:issueDate, testType:testType, result:result})
-    })
-    if(wait){
-      return(
-        <View style={{flex:1, justifyContent:'center', backgroundColor:'#efeff5'}}>
-          <ActivityIndicator size='large' color='rgb(0, 103, 187)' />
-        </View>
-      )
+  const onSelect = React.useCallback(
+    (id, authorityName, issueDate, testType, result) => {
+      navigation.navigate("Summary", {
+        id: id,
+        authority: authorityName,
+        issueDate: issueDate,
+        testType: testType,
+        result: result,
+      });
     }
-    else {
-      return(
-          <SafeAreaView style={{flex:1, backgroundColor:'#efeff5'}}>
-              <Text style={{fontSize:22, textAlign:'center', marginTop:20}}>Certificate History</Text>
-              {cert?<FlatList
-                  data={cert}
-                  ItemSeparatorComponent={
-                    () => (
-                      <View
-                        style={
-                           { marginTop: 5 }
-                        }
-                      />
-                    )
-                  }
-                  ListHeaderComponent= { () => (
-                    <View
-                      style={{ paddingTop:10 }}
-                    />
-                  )}
-                  refreshControl={
-                    <RefreshControl
-                        colors={["#ff862f", "#FF652F"]}
-                        refreshing={refresh}
-                        onRefresh={onRefresh} />
-                  }
-                  renderItem={({item}) => (
-                      <Test
-                          id={item.testId}
-                          title={item.testType}
-                          date={item.issueDate}
-                          expiration={item.expireDate}
-                          result={item.result}
-                          onSelect={() => onSelect(item.testId, item.authority, item.issueDate, item.testType, item.result, item.status)}
-                      />
-                  )}
-                  keyExtractor={item=>item.testId}
+  );
+  if (wait) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: "#efeff5",
+        }}
+      >
+        <ActivityIndicator size="large" color="rgb(0, 103, 187)" />
+      </View>
+    );
+  } else {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#efeff5" }}>
+        <Text style={{ fontSize: 22, textAlign: "center", marginTop: 20 }}>
+          Certificate History
+        </Text>
+        {cert ? (
+          <FlatList
+            data={cert}
+            ItemSeparatorComponent={() => <View style={{ marginTop: 5 }} />}
+            ListHeaderComponent={() => <View style={{ paddingTop: 10 }} />}
+            renderItem={({ item }) => (
+              <Test
+                id={item.testId}
+                title={item.testType}
+                date={item.issueDate}
+                expiration={item.expireDate}
+                result={item.result}
+                onSelect={() =>
+                  onSelect(
+                    item.testId,
+                    item.authorityName,
+                    item.issueDate,
+                    item.testType,
+                    item.result
+                  )
+                }
               />
-          :
-          (
-          <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor: '#efeff5',}}>
-            <Image style={{width:48, height:70, opacity: 0.9, marginVertical:6}} source={require('../../images/summary.png')}  />
-            <Text style={{fontSize:20, color:'rgb(0,103,189)'}}>No Certifications Available!</Text>
+            )}
+            keyExtractor={(item) => item.testId}
+          />
+        ) : (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#efeff5",
+            }}
+          >
+            <Image
+              style={{ width: 48, height: 70, opacity: 0.9, marginVertical: 6 }}
+              source={require("../../images/summary.png")}
+            />
+            <Text style={{ fontSize: 20, color: "rgb(0,103,189)" }}>
+              No Certifications Available!
+            </Text>
           </View>
-          )
-        }
-        </SafeAreaView>
-
-      )
-    }
-}
+        )}
+      </SafeAreaView>
+    );
+  }
+};
 
 const mapStateToProps = (state) => ({
-  userToken: state.auth.userToken
-})
+  userToken: state.auth.userToken,
+});
 
-export default connect(mapStateToProps)(CertificateHistory)
+export default connect(mapStateToProps)(CertificateHistory);
